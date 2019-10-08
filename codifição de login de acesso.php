efe46e43455e5ac29edd019204dd0df3f2800bc4
SamuelDiogo <44264105+SamuelDiogo@users.noreply.github.com>
2019-10-08 09:28:26 -0300

    Add files via upload

---
+++ codifição de login de acesso.php
@@ -0,0 +1,26 @@
+<?php
+session_start();
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+$login = $_POST['nome'];
+$entrar = $_POST['entrar'];
+$senha = md5($_POST['senha']);
+$connect = mysqli_connect('localhost','root','','star');
+  if (isset($entrar)) {
+    $verifica = mysqli_query("SELECT * FROM usuario WHERE nome = '$login' AND senha = '$senha'");
+      if (mysqli_num_rows($verifica)<=0){
+        if($login==='admin' && $senha==='admin'){
+		/**/
+        setcookie("nome",$login);
+        header("Location:admin.php");
+      }else{
+		setcookie("nome",$login);
+        header("Location:index1.php");
+	  }
+	  }else{
+        echo"<script language='javascript' type='text/javascript'>
+        alert('Login e/ou senha incorretos');window.location
+        .href='login.php';</script>";
+        die();
+      }
+  }
+?>
